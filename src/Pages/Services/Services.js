d52c9898e9eb30ce1f4795a108bf591df69fdabe
Iqbaleco <iqbaleco70@gmail.com>
2022-11-11 00:30:43 +0600

    added some data

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -15,17 +15,23 @@ const Services = () => {
             .then(data => setLimitedServices(data))
     }, []);
 
-    console.log(limitedServices.length);
+    if (loading) {
+        return <div className='grid place-items-center py-20'>
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-600"></div>
+        </div>
+    }
 
 
     return (
-        <div>
-            {/* {
-                limitedServices.map(singleService => <ServiceShortInfo
-                    key={singleService._id}
-
-                ></ServiceShortInfo>)
-            } */}
+        <div className='my-10 grid place-items-center'>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    limitedServices?.map(singleService => <ServiceShortInfo
+                        key={singleService._id}
+                        singleService={singleService}
+                    ></ServiceShortInfo>)
+                }
+            </div>
         </div>
     );
 };
