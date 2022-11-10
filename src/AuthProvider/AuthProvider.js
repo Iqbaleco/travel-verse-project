dfcaa0a387efb998e4e1f916699832ec92acd5fb
Iqbaleco <iqbaleco70@gmail.com>
2022-11-10 17:49:49 +0600

    added some data

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -1,19 +1,44 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
-import app from '../Firebase/firebase.config'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../Firebase/firebase.config';
 
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-
 const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser)
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
     const authInfo = {
-        // user,
-        // loading,
-        // createUser,
-        // login
+        user,
+        loading,
+        createUser,
+        login
     }
 
     return (
